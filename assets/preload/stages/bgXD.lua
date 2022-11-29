7a235910acd7e3a0f50226159245382a8e30c3af
noctrox389 <114263655+noctrox389@users.noreply.github.com>
2022-11-28 23:15:09 -0400

    Add files via upload

---
+++ assets/preload/stages/bgXD.lua
@@ -0,0 +1,9 @@
+-- Stage Stuff
+function onCreate()
+	makeLuaSprite('nada', 'nada', -1200,-400);
+	setScrollFactor('nada', 0.3, 0.3);
+	scaleObject('nada', 2.95, 2.95)
+
+	addLuaSprite('nada', false);
+
+	end
